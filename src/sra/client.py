ef546ed0578472ab649d87b573aac79aa36680ee
Yakiyo <yakiyosama5855@gmail.com>
2023-02-21 14:51:35 +0000

    feat: more works

--- src/sra/client.py
+++ src/sra/client.py
@@ -1,6 +1,28 @@
+import json
+from requests import get
+from .structs import Endpoint
+
 class Client:
     """
     Base class to interact with the api
     """
-    def __init__(self) -> None:
-        pass
+    def __init__(self, key: str = None) -> None:
+        self.api_key = key
+        self._endpoints = []
+        self._loadEndpoints()
+
+    def _loadEndpoints(self) -> None:
+        """
+        Method to initialize the endpoint cache
+        """
+        r = get('https://some-random-api.ml/endpoints?format=json', timeout = 5)
+        data = json.loads(r.content)
+
+        for categories in data:
+            category = data[categories]
+            for endpoint in category:
+                self._endpoints.append(Endpoint(endpoint))
+
+    def fetch(path: str, query: dict = None) -> dict:
+        return {}
+
