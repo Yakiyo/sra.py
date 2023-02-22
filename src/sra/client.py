3bef21b6fe82fa7796fc7f4fcd2afd1a5c3194b9
Yakiyo <yakiyosama5855@gmail.com>
2023-02-22 11:12:38 +0000

    feat: fetch method

--- src/sra/client.py
+++ src/sra/client.py
@@ -25,9 +25,38 @@ class Client:
             for endpoint in category:
                 self._endpoints.append(Endpoint(endpoint))
 
-    def fetch(self, path: str, query: dict = None) -> dict:
-        ep = next((x for x in self._endpoints if x.path == path), None)
-        if ep is None:
+    def fetch(self, path: str, query: dict = None) -> dict | bytes:
+        """
+        Fetch from an endpoint
+        """
+        endpoint = next((x for x in self._endpoints if x.path == path), None)
+        if endpoint is None:
             raise 'Invalid endpoint provided'
-        return ep
+        
+        if query is not None and self.api_key is not None:
+            query['key'] = self.api_key
+        
+        self._validate_request(endpoint, query)
+        try:
+            res = get('https://some-random-api.ml/' + path, params = query, timeout = 5)
+            if res.ok is False:
+                raise 'Error when fetching from api. Got status code ' + res.status_code
+            ctype = 'application/json'
+            if 'content-type' in res.headers:
+                ctype = res.headers['content-type']
+
+            if 'application/json' in ctype:
+                out = res.json()
+                if 'error' in out:
+                    raise out['error']
+                return out
+            elif 'image' in ctype:
+                return res.content
+
+            raise 'Invalid content type received from api. Got ' + res
+        except:
+            raise
+
+    def _validate_request(self, endpoint: Endpoint, query: dict):
+        pass
 
